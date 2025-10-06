24b0ab3447d1ca0f85459971c2b9f616afa4a2b5
Jan Schöppach <jan.schoeppach@metax-gmbh.de>
2025-10-06 14:16:50 +0200

    Update SDL to v3.2.24

--- cpm/sdl.cmake
+++ cpm/sdl.cmake
@@ -1,9 +1,9 @@
 include(CPM)
 CPMAddPackage(
   NAME SDL3
-  VERSION 3.2.18
-  URL https://github.com/libsdl-org/SDL/archive/refs/tags/release-3.2.18.zip
-  URL_HASH SHA256=3b3bee14d2825297e242b838a592810bc36f650b0156614d8ad0a8cc1f6d6fb0
+  VERSION 3.2.24
+  URL https://github.com/libsdl-org/SDL/archive/refs/tags/release-3.2.24.zip
+  URL_HASH SHA256=a8d7517c2e8b9eab777bd0d78bda15dfc7f61538853d19600be876bd20c939df
   OPTIONS
     "SDL_STATIC ON"
     "SDL_SHARED OFF"
