6238300b296d81ee0e3fbe99090bbdf1059cd550
Jan Schöppach <github@spiderpig.de>
2026-03-23 23:44:53 +0100

    Don't apply g++ commandline flag to cc

--- cmake/compiler_warnings.cmake
+++ cmake/compiler_warnings.cmake
@@ -26,7 +26,7 @@ set(GCC_WARNINGS
 	-Wduplicated-cond # warn if if / else chain has duplicated conditions
 	-Wduplicated-branches # warn if if / else branches have duplicated code
 	-Wlogical-op # warn about logical operations being used where bitwise were probably wanted
-	-Wuseless-cast # warn if you perform a cast to the same type
+	"$<$<COMPILE_LANGUAGE:CXX>:-Wuseless-cast>" # warn if you perform a cast to the same type
 )
 
 if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
