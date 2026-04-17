a17d70476a78ef539470ccf23c760414f7e6107d
Jan Schöppach <jan.schoeppach@metax-gmbh.de>
2026-04-17 15:59:22 +0200

    Update csv-parser to v3.4.0

--- cpm/csv-parser.cmake
+++ cpm/csv-parser.cmake
@@ -1,9 +1,9 @@
 include(CPM)
 CPMAddPackage(
   NAME csv-parser
-  VERSION 3.2.0
-  URL https://github.com/vincentlaucsb/csv-parser/archive/refs/tags/3.2.0.zip
-  URL_HASH SHA256=5775eb28b0a0eaaf4ca9e55cf8226f1949ed03f4cba79fee9f2eb4ec5be461e7
+  VERSION 3.4.0
+  URL https://github.com/vincentlaucsb/csv-parser/archive/refs/tags/3.4.0.zip
+  URL_HASH SHA256=36b9b56f401faf25e89f75932ec9960aaf509e2260ef62b938ed721672b7dacb
 )
 
 if (csv-parser_ADDED)
